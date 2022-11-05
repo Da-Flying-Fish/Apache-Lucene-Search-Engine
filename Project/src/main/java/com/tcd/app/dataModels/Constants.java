a417d68fae2c0006f949f8e4e4f0210d095063ea
aishwaryats02 <aishwaryats02@gmail.com>
2022-11-05 20:33:59 +0000

    Adding properties in config.Properties

---
+++ Project/src/main/java/com/tcd/app/dataModels/Constants.java
@@ -0,0 +1,9 @@
+package com.tcd.app.dataModels;
+
+/**
+ * This Class can be used to store string literals
+ */
+
+public class Constants {
+
+}
--- Project/src/main/java/com/tcd/app/helper/PropertyHelper.java
+++ Project/src/main/java/com/tcd/app/helper/PropertyHelper.java
@@ -1,6 +1,7 @@
 package com.tcd.app.helper;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyHelper {
@@ -18,6 +19,25 @@ public class PropertyHelper {
         }
 
         return propertiesVal;
+    }
+
+    /**
+     * To Check The Working of Property helper Function
+     * @param args
+     */
+    public static void main(String[] args){
+
+        Properties prop = null;
+        try {
+            prop = readPropFile("Project/src/config.Properties");
+            System.out.println("DataSourcePath= "+prop.getProperty("SourceDataFolderPath"));
+            System.out.println("Index Folder ="+prop.get("IndexedDataFolderPath"));
+            System.out.println("Results Folder ="+prop.get("ResultDataFolderPath"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 
