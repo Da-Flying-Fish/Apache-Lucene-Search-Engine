fae6242fb970d0e157f199f26d66680c3e6a0fbb
saqmod <60119567+saqzari@users.noreply.github.com>
2022-11-06 22:19:12 +0000

    Created query parser in Parser.java

--- Project/src/main/java/com/tcd/app/handler/Parser.java
+++ Project/src/main/java/com/tcd/app/handler/Parser.java
@@ -1,6 +1,14 @@
 package com.tcd.app.handler;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import com.tcd.app.helper.PropertyHelper;
 
 public class Parser {
 
@@ -37,9 +45,76 @@ public class Parser {
     /**
      *Method To Parse  Query Documents
      */
-    public static ArrayList<HashMap<String,String>>queryParser(){
-
-        return new ArrayList<>();
+    public static ArrayList<HashMap<String,String>> queryParser(){
+    	ArrayList<HashMap<String, String>> queries = new ArrayList<HashMap<String,String>>();
+    	Properties prop = PropertyHelper.readPropFile("src/config.Properties");
+    	try {
+	    	BufferedReader br = new BufferedReader(new FileReader(prop.getProperty("QueryDataFilePath")));
+			String line = br.readLine();
+			while (line != null)  
+			{  
+			   if (line.equals("<top>"))
+			   {
+				   HashMap<String, String> entry = new HashMap<String, String>();
+				   String number = "";
+				   String title = "";
+				   String description = "";
+				   String narrative = "";
+				   while (!line.equals("</top>"))
+				   {
+					   boolean end = false;
+					   if (line.contains("<num>"))
+					   {
+						   String[] split = line.split("\\s+");
+						   number += split[2];
+					   }
+					   else if (line.contains("<title>"))
+					   {
+						   String[] split = line.split("\\s+");
+						   List<String> titleList = new ArrayList<String>();
+						   titleList.addAll(Arrays.asList(split));
+						   titleList.remove(0);
+						   title = String.join(" ", titleList);
+					   }
+					   else if(line.contains("<desc>"))
+					   {
+						   line = br.readLine();
+						   while(!line.contains("<narr>"))
+						   {
+							   if(!description.equals(""))
+								   description += " ";
+							   if (!line.equals(""))
+								   description += line.trim();
+							   line = br.readLine();
+						   }
+						   
+						   line = br.readLine();
+						   while(!line.contains("</top>"))
+						   {
+							   if(!narrative.equals(""))
+								   narrative += " ";
+							   if (!line.equals(""))
+								   narrative += line.trim();
+							   line = br.readLine();
+						   }
+						   end = true;  
+					   }
+					   
+					   if(!end)
+						   line = br.readLine();  
+				   }
+				   entry.put("number", number);
+				   entry.put("title", title);
+				   entry.put("description", description);
+				   entry.put("narrative", narrative);
+				   queries.add(entry);
+			   }
+			   line = br.readLine();  
+			} 
+			br.close();
+    	} catch (IOException e) {
+			e.printStackTrace();
+		}  
+    	return queries;
     }
-
 }
