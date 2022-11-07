421e2e790a6ebd122dfbc016a29be35f1e8e9078
FaceTcd1997 <117104430+FaceTcd1997@users.noreply.github.com>
2022-11-07 17:15:35 +0000

    FTparser + pom java 16 compiler integration

--- Project/src/main/java/com/tcd/app/handler/Parser.java
+++ Project/src/main/java/com/tcd/app/handler/Parser.java
@@ -1,6 +1,8 @@
 package com.tcd.app.handler;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
 
 public class Parser {
 
@@ -8,10 +10,94 @@ public class Parser {
      * Method To Parse Documents realted to financial Times Limited
      */
 
-    public static ArrayList<HashMap<String,String>> fTParser(){
-        return new ArrayList<>();
+    public static List<Map<String,String>> fTParser(){
+        List<File> collection = new ArrayList<>();
+        List<Map<String, String>> docList = new ArrayList<>();
 
+        File dir = new File("Project/resources/DataSource/ft");
+        getDocuments(Objects.requireNonNull(dir.listFiles()), collection);
+
+        for(File file: collection){
+            try{
+                FileReader fileReader = new FileReader(file);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line = "";
+
+                String attribute = "";
+                String content = "";
+                Map<String, String> document = null;
+
+                while ((line = reader.readLine()) != null) {
+                    //Start of the document
+                    if(line.equals("<DOC>")){
+                        attribute = "";
+                        content = "";
+                        document = new HashMap<>();
+                    }
+                    //End of the document
+                    else if(line.equals("</DOC>")){
+                        docList.add(document);
+                    }else{
+                        String[] elements = line.split("<|>");
+                        //TAG start and TAG end in the same line
+                        if(elements.length == 4){
+                            attribute = elements[1];
+                            content = elements[2];
+                            document.put(attribute, content.strip());
+
+                            attribute = "";
+                            content = "";
+                        }
+                        //TAG start + value or value + TAG end
+                        else if(elements.length == 3){
+                            if(elements[2].contains("/")){
+                                content = content.concat(elements[1]);
+                                document.put(attribute, content.strip());
+
+                                attribute = "";
+                                content = "";
+                            }else {
+                                attribute = elements[1];
+                                content = elements[2];
+                            }
+                        }
+                        else if(elements.length == 2){
+                            if(elements[1].contains("/")){
+                                document.put(attribute, content.strip());
+
+                                attribute = "";
+                                content = "";
+                            }else{
+                                attribute = elements[1];
+                                content = "";
+                            }
+                        }
+                        else{
+                            content = content.concat("").concat(elements[0]).concat(" ");
+                        }
+                    }
+                }
+            }catch (Exception e){
+                System.out.println("ERROR PARSING DOCUMENTS - FT_Collection");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        return docList;
     }
+
+    public static List<File> getDocuments(File[] files, List<File> collection){
+        for(File file : files){
+            if(file.isDirectory()){
+                collection.addAll(Arrays.stream(Objects.requireNonNull(file.listFiles())).toList());
+            }else{
+                collection.add(file);
+            }
+        }
+        return collection;
+    }
+
     /***
      * Method To Parse Documents realted to Federal Register
      */
