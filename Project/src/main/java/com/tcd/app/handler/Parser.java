f6abdba54fe051bdd7aa738f639245eb29d1d168
root <mugheesh@tcd.ie>
2022-11-10 01:48:03 +0000

    Added array list with doc maps and progress bar

--- Project/src/main/java/com/tcd/app/handler/Parser.java
+++ Project/src/main/java/com/tcd/app/handler/Parser.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Arrays;
 
+import java.lang.StringBuilder;
+import java.lang.Math;
+import java.lang.OutOfMemoryError;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -41,10 +45,18 @@ public class Parser {
     public static ArrayList<HashMap<String,String>> lATParser() {
 		String latDirPath = "resources/DataSource/latimes/";
 
+		ArrayList<HashMap<String, String>> docMapList = new ArrayList<>();
+
 		File files[] = new File(latDirPath).listFiles();
+		int n = files.length;
+
+		System.out.println("Parsing LA Times data...");
 
 		// Iterate each file in the "latimes" directory
-		for (File f : files) {
+		for (int i=0 ; i<n ; i++) {
+			progressBar(i, n);
+
+			File f = files[i];
 			Path fPath = Paths.get(f.getPath());
 
 			try {
@@ -96,18 +108,18 @@ public class Parser {
 						keyFlag = false;
 					}
 
-					System.out.println(docMap);
-
-					/*
-						TODO: Add the hashmap to a list of hashmaps
-					*/
+					docMapList.add(docMap);
 				}
-			} catch (IOException e) {
-				System.out.println(e);
+			} catch (IOException ioe) {
+				System.err.println(ioe);
+			} catch (OutOfMemoryError oome) {
+				// System.err.println("Max JVM memory: " + Runtime.getRuntime().maxMemory());
 			}
 		}
 
-        return new ArrayList<>();
+		System.out.println("\nCOMPLETE!");
+
+        return docMapList;
     }
     /**
      *Method To Parse  Query Documents
@@ -117,4 +129,18 @@ public class Parser {
         return new ArrayList<>();
     }
 
+	private static void progressBar(int progress, int goal) {
+		int percentage = Math.round(((float)progress/(float)goal)*100);
+
+		StringBuilder complete = new StringBuilder("");
+		StringBuilder incomplete = new StringBuilder();
+
+		for (int i=0 ; i<percentage ; i++)
+			complete.append("█");
+
+		for (int i=100 ; i>=percentage ; i--)
+			incomplete.append("░");
+
+		System.out.print(complete.toString() + incomplete.toString() + " " + percentage + "%" + "\r");
+	}
 }
