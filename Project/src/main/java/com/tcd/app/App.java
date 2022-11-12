4fce2c1f4bf5bdbd05fa524de50380a0222ce001
KSK-max <67270061+KSK-max@users.noreply.github.com>
2022-11-12 13:48:57 +0000

    Ft parser refactoring

--- Project/src/main/java/com/tcd/app/App.java
+++ Project/src/main/java/com/tcd/app/App.java
@@ -1,4 +1,10 @@
 package com.tcd.app;
+
+import com.tcd.app.handler.Parser;
+import com.tcd.app.helper.PropertyHelper;
+
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -7,6 +13,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
-}
+        String propertyFilePath="Project/src/config.Properties";
+        Properties properties = PropertyHelper.readPropFile(propertyFilePath);
+        Parser.fTParser(properties);
+
+    }}
---
+++ Project/src/main/java/com/tcd/app/helper/Utilities.java
@@ -0,0 +1,30 @@
+package com.tcd.app.helper;
+
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utilities {
+
+    public static List<File> getFilesInDir(String rootFolderPath){
+
+        List<File> filesInDirectory = new ArrayList<>();
+        try {
+            try (DirectoryStream<Path> stream = Files
+                    .newDirectoryStream(Paths.get(rootFolderPath))) {
+                for (Path path : stream) {
+                    if (!Files.isDirectory(path)) {
+                        filesInDirectory.add(path.toFile());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filesInDirectory;
+    }
+}
